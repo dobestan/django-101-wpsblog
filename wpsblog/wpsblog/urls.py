77737da9058bcc0b94f4595db2f614d6ed0dae8b
Suchan An <me@ansuchan.com>
2016-05-24 16:46:17 +0900

    add room controller: get room_id from url and returns text with room_id

--- wpsblog/wpsblog/urls.py
+++ wpsblog/wpsblog/urls.py
@@ -8,8 +8,13 @@ def home(request):
     return HttpResponse("hello world")
 
 
+def room(request, room_id):
+    return HttpResponse("This is a room detail " + room_id)
+
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
 
     url(r'^$', home),
+    url(r'^rooms/(?P<room_id>\d+)/$', room),
 ]

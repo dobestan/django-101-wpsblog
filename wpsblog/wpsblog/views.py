9c79e2ab5c091a7efa8522cc09a52dc28aa36ee8
Suchan An <me@ansuchan.com>
2016-05-24 17:19:34 +0900

    update room view: add content_type application/json to response header

--- wpsblog/wpsblog/views.py
+++ wpsblog/wpsblog/views.py
@@ -10,5 +10,8 @@ def home(request):
 def room(request, room_id):
     url = "https://api.zigbang.com/v1/items?detail=true&item_ids=" + room_id
     response = requests.get(url)
-    return HttpResponse(response.content)
+    return HttpResponse(
+        response.content,
+        content_type="application/json",
+    )
 

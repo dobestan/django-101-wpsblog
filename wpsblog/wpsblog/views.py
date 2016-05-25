61289a7b134dfa0fb09089aee0571249610a4b2f
Suchan An <me@ansuchan.com>
2016-05-25 14:59:02 +0900

    update news view: replace count, news_content variable

--- wpsblog/wpsblog/views.py
+++ wpsblog/wpsblog/views.py
@@ -33,16 +33,20 @@ def news(request):
 
     with open(settings.BASE_DIR + "/wpsblog/templates/news.html", "r") as template:
         content = template.read()
-        content += "<p>{count} 개의 영화 뉴스 정보가 있습니다.</p>".format(count=len(news_list)) +\
-            "".join([
-                "<h2>{title}</h2><img src={image_src}><p>{content}</p>".format(
-                    title=news.get('title'),
-                    image_src=news.get('image'),
-                    content=news.get('content'),
-                )
-                for news
-                in news_list
-            ])
+
+        count = len(news_list)
+        news_content = "".join([
+            "<h2>{title}</h2><img src={image_src}><p>{content}</p>".format(
+                title=news.get('title'),
+                image_src=news.get('image'),
+                content=news.get('content'),
+            )
+            for news
+            in news_list
+        ])
+
+        content = content.replace("## count ##", str(count))
+        content = content.replace("## news_content ##", news_content)
 
         return HttpResponse(
             content,

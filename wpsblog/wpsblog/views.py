de7d00892ca063833ad19559a34da046b214e817
Suchan An <me@ansuchan.com>
2016-05-25 14:44:34 +0900

    refactor news view: seperate html code from python script

--- wpsblog/wpsblog/views.py
+++ wpsblog/wpsblog/views.py
@@ -2,6 +2,7 @@ import json
 import requests
 
 from django.http.response import HttpResponse
+from django.conf import settings
 
 
 def home(request):
@@ -24,33 +25,25 @@ def news(request):
     news_dict = json.loads(response.text)
     news_list = news_dict.get("news")
 
-    form_html = """
-        <form method="GET" action="/news/">
-            <input type="text" name="search">
-            <input type="submit" value="검색">
-        </form>
-    """
-
     if search:
         news_list = list(filter(
             lambda news: search in news.get('title'),
             news_list,
         ))
 
-    content = "<h1>News</h1>" +\
-        "<p>This is news page.</p>" +\
-        form_html +\
-        "<p>{count} 개의 영화 뉴스 정보가 있습니다.</p>".format(count=len(news_list)) +\
-        "".join([
-            "<h2>{title}</h2><img src={image_src}><p>{content}</p>".format(
-                title=news.get('title'),
-                image_src=news.get('image'),
-                content=news.get('content'),
-            )
-            for news
-            in news_list
-        ])
+    with open(settings.BASE_DIR + "/wpsblog/templates/news.html", "r") as template:
+        content = template.read()
+        content += "<p>{count} 개의 영화 뉴스 정보가 있습니다.</p>".format(count=len(news_list)) +\
+            "".join([
+                "<h2>{title}</h2><img src={image_src}><p>{content}</p>".format(
+                    title=news.get('title'),
+                    image_src=news.get('image'),
+                    content=news.get('content'),
+                )
+                for news
+                in news_list
+            ])
 
-    return HttpResponse(
-        content,
-    )
+        return HttpResponse(
+            content,
+        )

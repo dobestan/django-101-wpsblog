fb042b6edefd38ec44fb3805412d7c94891a7659
Suchan An <me@ansuchan.com>
2016-05-30 14:50:47 +0900

    refactor urls.py: group policy urls using include, and namespace

--- wpsblog/wpsblog/urls.py
+++ wpsblog/wpsblog/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
 
 from wpsblog.views import *
@@ -12,7 +12,9 @@ urlpatterns = [
     url(r'^rooms/(?P<room_id>\d+)/$', room, name="room"),
     url(r'^news/$', news, name="news"),
 
-    url(r'^policy/terms/$', terms, name="terms"),
-    url(r'^policy/privacy/$', privacy, name="privacy"),
-    url(r'^policy/disclaimer/$', disclaimer, name="disclaimer"),
+    url(r'^policy/', include([
+        url(r'terms/$', terms, name="terms"),
+        url(r'privacy/$', privacy, name="privacy"),
+        url(r'disclaimer/$', disclaimer, name="disclaimer"),
+    ], namespace="policy")),
 ]

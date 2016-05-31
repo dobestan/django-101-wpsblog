59b380a6a1152c44534922169b7c27246fef962d
Suchan An <me@ansuchan.com>
2016-05-31 17:30:17 +0900

    update Post model: implement get_absolute_url

--- wpsblog/wpsblog/models.py
+++ wpsblog/wpsblog/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.urlresolvers import reverse
 
 
 class Post(models.Model):
@@ -9,3 +10,11 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse(
+            "post-detail",
+            kwargs={
+                "post_id": self.id,
+            }
+        )

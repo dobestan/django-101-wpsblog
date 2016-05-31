5f1ddf820e561f9502861de3393fb97e6ff23245
Suchan An <me@ansuchan.com>
2016-05-31 17:01:42 +0900

    update posts list view

--- wpsblog/wpsblog/views/posts/list.py
+++ wpsblog/wpsblog/views/posts/list.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
 
+from wpsblog.models import Post
+
 
 def list(request):
     return render(
         request,
         "posts/list.html",
-        {},
+        {
+            "posts": Post.objects.all(),
+        },
     )

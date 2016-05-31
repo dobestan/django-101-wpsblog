2748fda775484727cc4c34ca213b74b8cc56bd28
Suchan An <me@ansuchan.com>
2016-05-31 17:10:33 +0900

    update posts detail view

--- wpsblog/wpsblog/views/posts/detail.py
+++ wpsblog/wpsblog/views/posts/detail.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
 
+from wpsblog.models import Post
+
 
 def detail(request, post_id):
     return render(
         request,
         "posts/detail.html",
-        {},
+        {
+            "post": Post.objects.get(id=post_id),
+        },
     )

69572950daa17ceb37999e7d4612d2b0536eb6ee
Suchan An <me@ansuchan.com>
2016-05-31 16:22:32 +0900

    init Post model: Post has a title, and content

---
+++ wpsblog/wpsblog/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+
+class Post(models.Model):
+    title = models.CharField(
+        max_length=120,
+    )
+    content = models.TextField()
+
+    def __str__(self):
+        return self.title

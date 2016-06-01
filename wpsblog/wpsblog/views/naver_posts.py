fba59e305a20c086362a9ab767a2bcece9e1eecc
Suchan An <me@ansuchan.com>
2016-06-01 14:58:56 +0900

    update naver_posts list view: add keyword search feature

--- wpsblog/wpsblog/views/naver_posts.py
+++ wpsblog/wpsblog/views/naver_posts.py
@@ -4,10 +4,22 @@ from wpsblog.models import NaverPost
 
 
 def naver_posts_list(request):
+    keyword = request.GET.get("keyword")
+    naver_posts = NaverPost.objects.all()
+
+    if keyword:
+        naver_posts = [
+            naver_post
+            for naver_post
+            in naver_posts
+            if naver_post.keyword == keyword
+        ]
+
     return render(
         request,
         "naver_posts/list.html",
         {
-            "naver_posts": NaverPost.objects.all(),
+            "keyword": keyword,
+            "naver_posts": naver_posts,
         },
     )

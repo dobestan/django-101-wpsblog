4d5871414b2bcffe751ea09bdba0c97922c20f82
Suchan An <me@ansuchan.com>
2016-06-07 16:08:44 +0900

    update Comment: add html id for get_absolute_url

--- wpsblog/wpsblog/models/comment.py
+++ wpsblog/wpsblog/models/comment.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.urlresolvers import reverse
 
 
 class Comment(models.Model):
@@ -9,3 +10,11 @@ class Comment(models.Model):
 
     def __str__(self):
         return self.content
+
+    def get_absolute_url(self):
+        return reverse(
+            "posts:detail",
+            kwargs={
+                "post_id": self.post.id,
+            },
+        ) + "#comment-" + str(self.id)

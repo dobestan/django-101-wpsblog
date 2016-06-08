b3af58b5fdb014556e94abba27e407ffbcf82abd
Suchan An <me@ansuchan.com>
2016-06-08 16:59:29 +0900

    init auth views: login, signup

--- wpsblog/wpsblog/views/auth/login.py
+++ wpsblog/wpsblog/views/auth/login.py
@@ -1,7 +1,20 @@
-from django.shortcuts import render
+from django.core.urlresolvers import reverse
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login as auth_login
 
 
 def login(request):
+    if (request.method == "POST"):
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+
+        user = authenticate(username=username, password=password)
+
+        if user:
+            auth_login(request, user)
+            return redirect(reverse("home"))
+        return redirect(reverse("auth:login"))
+
     return render(
         request,
         "auth/login.html",
--- wpsblog/wpsblog/views/auth/signup.py
+++ wpsblog/wpsblog/views/auth/signup.py
@@ -1,7 +1,22 @@
-from django.shortcuts import render
+from django.core.urlresolvers import reverse
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
 
 
 def signup(request):
+    if (request.method == "POST"):
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+        email = request.POST.get("email")
+
+        user = User.objects.create_user(
+            username=username,
+            password=password,
+            email=email,
+        )
+
+        return redirect(reverse("auth:login"))
+
     return render(
         request,
         "auth/signup.html",

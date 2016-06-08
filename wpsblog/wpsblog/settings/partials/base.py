b39b392eba37bd27832579ce6fd686b659e8dded
Suchan An <me@ansuchan.com>
2016-06-08 15:17:10 +0900

    update media settings: add MEDIA_ROOT

--- wpsblog/wpsblog/settings/partials/base.py
+++ wpsblog/wpsblog/settings/partials/base.py
@@ -3,6 +3,7 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))))
+PROJECT_ROOT_DIR = os.path.dirname(BASE_DIR)
 
 
 # Quick-start development settings - unsuitable for production
--- wpsblog/wpsblog/settings/partials/static.py
+++ wpsblog/wpsblog/settings/partials/static.py
@@ -1,4 +1,17 @@
+import os
+
+from .base import PROJECT_ROOT_DIR
+
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+# Media files
+MEDIA_ROOT = os.path.join(
+    PROJECT_ROOT_DIR,
+    "dist",
+    "media",
+)

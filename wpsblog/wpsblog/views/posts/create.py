6330598cff9be23ce235893673fb6c02d26d0cfa
Suchan An <me@ansuchan.com>
2016-06-08 15:05:56 +0900

    update posts create form: change enctype to multipart

--- wpsblog/wpsblog/views/posts/create.py
+++ wpsblog/wpsblog/views/posts/create.py
@@ -6,10 +6,12 @@ from wpsblog.models import Post
 def create(request):
     title = request.POST.get("title")
     content = request.POST.get("content")
+    image = request.FILES.get("image")
 
     post = Post.objects.create(
         title=title,
         content=content,
+        image=image,
     )
 
     return redirect(post)

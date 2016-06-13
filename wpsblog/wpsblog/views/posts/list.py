38bfa3a29d596fa82b9d2740871e8cfb893945f0
Suchan An <me@ansuchan.com>
2016-06-13 16:53:24 +0900

    update PostListView: add pagination feature

--- wpsblog/wpsblog/views/posts/list.py
+++ wpsblog/wpsblog/views/posts/list.py
@@ -1,4 +1,5 @@
 from django.views.generic.list import ListView
+from django.core.paginator import Paginator
 
 from wpsblog.models import Post
 
@@ -8,3 +9,6 @@ from .base import PostBaseView
 class PostListView(PostBaseView, ListView):
     template_name = "posts/list.html"
     context_object_name = "posts"
+
+    def get_paginate_by(self, queryset):
+        return self.request.GET.get("per", 5)

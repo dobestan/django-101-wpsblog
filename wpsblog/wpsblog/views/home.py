8a8efdf6175f56bd395e541711d6b75f93642486
Suchan An <me@ansuchan.com>
2016-06-13 15:25:44 +0900

    update HomeView: refactor via TemplateView

--- wpsblog/wpsblog/views/home.py
+++ wpsblog/wpsblog/views/home.py
@@ -1,12 +1,12 @@
 from django.shortcuts import render
-from django.views.generic import View
+from django.views.generic import TemplateView
 
 
-class HomeView(View):
+class HomeView(TemplateView):
+    template_name = "home.html"
 
-    def get(self, request, *args, **kwargs):
-        return render(
-            request,
-            "home.html",
-            {"site_name": "wps blog"},
-        )
+    def get_context_data(self, **kwargs):
+        context = super(HomeView, self).get_context_data(**kwargs)
+
+        context["site_name"] = "wps blog"
+        return context

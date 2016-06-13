cf8e8c8a46b37bbeab82a4de39529c5c75b083ac
Suchan An <me@ansuchan.com>
2016-06-13 13:17:03 +0900

    update login: add next_page functionality

--- wpsblog/wpsblog/views/auth/login.py
+++ wpsblog/wpsblog/views/auth/login.py
@@ -7,12 +7,13 @@ def login(request):
     if (request.method == "POST"):
         username = request.POST.get("username")
         password = request.POST.get("password")
+        next_page = request.POST.get("next_page") or reverse("auth:mypage")
 
         user = authenticate(username=username, password=password)
 
         if user:
             auth_login(request, user)
-            return redirect(reverse("home"))
+            return redirect(next_page)
         return redirect(reverse("auth:login"))
 
     return render(

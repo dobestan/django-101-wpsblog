680775c6e477bf2a51a247f85583afabb9f7a749
Suchan An <me@ansuchan.com>
2016-06-14 14:20:48 +0900

    update PostCreateView: via CreateView

--- wpsblog/wpsblog/views/posts/create.py
+++ wpsblog/wpsblog/views/posts/create.py
@@ -1,21 +1,17 @@
-from django.views.generic import View
-from django.shortcuts import redirect
+from django.views.generic.edit import CreateView
 
 from wpsblog.models import Post
 
+from .base import PostBaseView
 
-class PostCreateView(View):
 
-    def post(self, request, *args, **kwargs):
-        title = request.POST.get("title")
-        content = request.POST.get("content")
-        image = request.FILES.get("image")
+class PostCreateView(PostBaseView, CreateView):
+    fields = [
+        "title",
+        "content",
+        "image",
+    ]
 
-        post = Post.objects.create(
-            user=request.user,
-            title=title,
-            content=content,
-            image=image,
-        )
-
-        return redirect(post)
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super(PostCreateView, self).form_valid(form)

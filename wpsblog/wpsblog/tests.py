191b26fe2d9dc2ad3a2f514e62c4b810b6ccb851
Suchan An <me@ansuchan.com>
2016-06-15 14:33:31 +0900

    init HomeViewTestCase: test home status_code should return 200 OK

--- wpsblog/wpsblog/tests.py
+++ wpsblog/wpsblog/tests.py
@@ -2,6 +2,18 @@ from django.test import TestCase
 from django.core.urlresolvers import reverse
 
 
+class HomeViewTestCase(TestCase):
+
+    def setUp(self):
+        self.response = self.client.get(reverse("home"))
+
+    def test_home_view_should_return_200(self):
+        self.assertEqual(
+            self.response.status_code,
+            200,
+        )
+
+
 class PricingViewTestCase(TestCase):
 
     def setUp(self):

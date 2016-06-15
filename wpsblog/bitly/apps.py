fcab18eac3fd9cbcc73967712ea548fab1eff1d0
Suchan An <me@ansuchan.com>
2016-06-15 15:31:04 +0900

    fix failing test

--- wpsblog/bitly/apps.py
+++ wpsblog/bitly/apps.py
@@ -5,5 +5,4 @@ class BitlyAppConfig(AppConfig):
     name = "bitly"
 
     def ready(self):
-        # from bitly.signals.post_save import post_save_bitlink
-        pass
+        from bitly.signals.post_save import post_save_bitlink

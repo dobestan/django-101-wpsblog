d35e887de386fc980d9d8a391b16850bdeac7ced
Suchan An <me@ansuchan.com>
2016-06-15 17:04:04 +0900

    update signup: add phonenumber, address

--- wpsblog/wpsblog/views/auth/signup.py
+++ wpsblog/wpsblog/views/auth/signup.py
@@ -17,11 +17,17 @@ class SignupView(View):
         username = request.POST.get("username")
         password = request.POST.get("password")
         email = request.POST.get("email")
+        phonenumber = request.POST.get("phonenumber")
+        address = request.POST.get("address")
 
         user = User.objects.create_user(
             username=username,
             password=password,
             email=email,
         )
+
+        user.userprofile.phonenumber = phonenumber
+        user.userprofile.address = address
+        user.userprofile.save()
 
         return redirect(reverse("auth:login"))
